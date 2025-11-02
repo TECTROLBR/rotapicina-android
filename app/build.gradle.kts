0c629535a754e4ac45d31b0e23fae25d9a8b1dd6
Denys dos Santos Mota <denysmota@escoladotrabalhador40.com.br>
2025-11-02 15:19:45 -0300

    Versão inicial do aplicativo de tarefas *adicionado funçao extras como horario * imagem para um melhor visual * prioridades com horarios * novas funçao como linta tarefas concluidas ao fina da semana para ela inciar linpas * nova persolalizaçao com imagens e destaque na lista de tarefas * novas funçao de saldaçao muda de acordo com horaria visto * remoçao de bug de navegaçao * e atualizaçao do app para v1.0.1 * novos erros removido * correçao de bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,23 +42,25 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("com.google.code.gson:gson:2.13.2")
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.gson)
+    implementation(libs.material)
+    implementation(platform(libs.androidx.compose.bom))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
