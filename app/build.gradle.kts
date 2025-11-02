e9a2d5366610863451dc35ce06423cf1e39ef13c
Denys dos Santos Mota <denysmota@escoladotrabalhador40.com.br>
2025-11-02 16:10:30 -0300

    Versão inicial do aplicativo de tarefas *adicionado funçao extras como horario * imagem para um melhor visual * prioridades com horarios * novas funçao como linta tarefas concluidas ao fina da semana para ela inciar linpas * nova persolalizaçao com imagens e destaque na lista de tarefas * novas funçao de saldaçao muda de acordo com horaria visto * remoçao de bug de navegaçao * e atualizaçao do app para v1.0.1 * novos erros removido * correçao de bugs *novos erros corrigidos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.rotapicina"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
